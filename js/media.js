7c5cbec412c085bda54f6ed661411b8961aadccf
Hugoleung04 <Hugoleung04@users.noreply.github.com>
2026-08-30 11:37:49 +0000

    Fix iPhone picker Done/tick: move file inputs out of overflow containers

--- js/media.js
+++ js/media.js
@@ -185,7 +185,25 @@
     });
   }
 
+  function openPicker(id) {
+    const el = $(id);
+    if (!el) return;
+    el.value = "";
+    el.click();
+  }
+
   function bind() {
+    const pairs = [
+      ["photoBtn", "photoInput"],
+      ["videoBtn", "videoInput"],
+      ["cameraPhotoBtn", "cameraPhotoInput"],
+      ["cameraVideoBtn", "cameraVideoInput"],
+      ["glbBtn", "glbInput"]
+    ];
+    pairs.forEach(([btnId, inputId]) => {
+      const btn = $(btnId);
+      if (btn) btn.addEventListener("click", () => openPicker(inputId));
+    });
     hook("photoInput", (e) => {
       addFiles(e.target.files, "上傳相片");
       e.target.value = "";
